ead9515cc9b70f27d3fee18668218d35fd26ada1
Michael Jenkins <michael.jenkins@disney.com>
2017-11-10 17:49:53 -0800

    Fix view reference

--- urls.py
+++ urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.bare, name='bootstrap_template'),
+    url(r'^$', views.bootstrap_template, name='bootstrap_template'),
 ]
